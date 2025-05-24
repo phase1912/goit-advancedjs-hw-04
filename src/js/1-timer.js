d5f7dd8a01933775f4c6640efde3a2c65ce73225
Bohdan <phase1912@gmail.com>
2025-05-24 20:11:05 +0200

    added code

---
+++ src/js/1-timer.js
@@ -0,0 +1,84 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+let userSelectedDate = null;
+const startBtn = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (userSelectedDate <= Date.now()) {
+      //window.alert("Please choose a date in the future");
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  let ms = userSelectedDate - Date.now();
+  const timer = setInterval(() => {
+    const { days, hours, minutes, seconds } = convertMs(ms);
+    ms -= 1000;
+    if (ms <= 0) {
+      clearInterval(timer);
+      updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+    }
+
+    updateTimerDisplay({ days, hours, minutes, seconds });
+  }, 1000);
+});
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  daysSpan.textContent = String(days).padStart(2, '0');
+  hoursSpan.textContent = String(hours).padStart(2, '0');
+  minutesSpan.textContent = String(minutes).padStart(2, '0');
+  secondsSpan.textContent = String(seconds).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
